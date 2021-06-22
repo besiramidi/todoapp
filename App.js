9c1b3cfe572c23fd2a9206e8dcaba5154857e927
besiramidi <besiramidi0@gmail.com>
2021-06-22 17:32:50 +0200

    completed part 5

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Alert , View ,FlatList ,} from 'react-native';
+import { StyleSheet, Alert , View ,FlatList , TouchableWithoutFeedback , Keyboard ,} from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
@@ -34,20 +34,25 @@ export default function App() {
   }
 
   return (
+  <TouchableWithoutFeedback onPress={() => {
+    Keyboard.dismiss();
+    console.log('dismissed keyboard');
+  }}>
     <View style={styles.container}>
-    <Header/>
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler}/>
-        <View style={styles.list}>
-          <FlatList 
-            data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
-           />
+      <Header/>
+        <View style={styles.content}>
+          <AddTodo submitHandler={submitHandler}/>
+          <View style={styles.list}>
+            <FlatList 
+              data={todos}
+              renderItem={({ item }) => (
+                <TodoItem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
         </View>
-      </View>
     </View>
+  </TouchableWithoutFeedback>
   );
 }
 
